de9ab2d63784ce3a6a1993c95d32b8edfec48b24
romik <tarkin20@yandex.ru>
2021-03-11 00:00:32 +0500

    Главная страница (светлая и тёмная тема)

---
+++ light/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Romtuck</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+  </head>
+<body>
+
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
+ <p class=" h5 my-0 me-md-auto fw-normal">Romtuck</p>
+    <nav class="my-2 my-md-0 me-md-3">
+      <a class="p-2 text-dark" href="index.php">Проекты</a>
+      <a class="p-2 text-dark" href="https://vk.com/romtuck">Мой вк</a>
+      <a class="p-2 text-dark" href="#">Поддержка</a>
+      <a class="p-2 text-dark" href="#">Сертификаты</a>
+      <a class="btn btn-outline-primary" href="#">Войти</a>
+    </nav>
+</header>
+
+
+<main class="container">
+  <div class="col-md-12">
+  <h1>Проекты</h1>
+  <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">World</strong>
+          <h3 class="mb-0">Featured post</h3>
+          <div class="mb-1 text-muted">Nov 12</div>
+          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+      </div>
+      
+
+    </div>
+</main>
+
+
+
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
