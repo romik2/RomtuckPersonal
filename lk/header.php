a3e127325e563e72d5336b2c4201b9f9cba2718e
romik <tarkin20@yandex.ru>
2021-03-11 11:26:02 +0500

    Управление проектами

--- lk/header.php
+++ lk/header.php
@@ -1,43 +1,34 @@
-<!doctype html>
-<html class="no-js" lang="ru">
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Romtuck</title>
-    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
 
-    <body>
-    
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
+<div class="col-md-3">
+  <a class="navbar-brand col-md-6 col-lg-3 me-0 px-3" href="#">Romtuck</a>
 
-    <div class="col-md-3">
-      <a class="navbar-brand col-md-6 col-lg-3 me-0 px-3" href="#">Romtuck</a>
-   
-      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      </div>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  </div>
 
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <div class="nav-link">Выйти</div>
-        </li>
-      </ul>
-    </header>
-    
-    <div class="container-fluid">
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <div class="nav-link">Выйти</div>
+    </li>
+  </ul>
+</header>
+
+
+<div class="container-fluid">
       <div class="row">
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
           <div class="position-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <a class="nav-link active" aria-current="page" href="index.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                   Главная
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="project.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                   Проекты
                 </a>
@@ -63,43 +54,3 @@
             </ul>
           </div>
         </nav>
-    
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Главная</h1>
-          </div>
-    
-        
-          <h2>Последние действия</h2>
-          <div class="table-responsive">
-            <table class="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Действие</th>
-                  <th>Дата</th>
-                  <th>Пользователь</th>
-                </tr>
-              </thead>
-              <tbody>
-
-                <tr>
-                  <td>1</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                </tr>
-
-              </tbody>
-            </table>
-          </div>
-        </main>
-      </div>
-    </div>
-    
-    </body>
-
-<script src="../../js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
-
-</html>
---
+++ lk/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Romtuck</title>
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+
+    <body>
+    
+        <?php include_once('header.php')?>
+   
+    
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Главная</h1>
+          </div>
+    
+        
+          <h2>Последние действия</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Действие</th>
+                  <th>Дата</th>
+                  <th>Пользователь</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr>
+                  <td>1</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+    
+    </body>
+
+<script src="../../js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+
+</html>
