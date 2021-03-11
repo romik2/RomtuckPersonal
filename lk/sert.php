0d44b67f43a89c2c2f9a3fbcbe89bc609b207a2a
romik <tarkin20@yandex.ru>
2021-03-11 11:31:16 +0500

    Управление сертификатами

---
+++ lk/sert.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Romtuck</title>
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+
+    <body>
+    
+        <?php include_once('header.php')?>
+   
+    
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Управление сертификатами</h1>
+          </div>
+        <a class="btn btn-outline-primary" href="login.php">Добавить</a>
+        
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Название</th>
+                  <th>Ссылка на картинку</th>
+                  <th>Дата публикации</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr>
+                  <td>1</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+    
+    </body>
+
+<script src="../../js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+
+</html>
