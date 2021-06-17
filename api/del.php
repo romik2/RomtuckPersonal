7a56baca892c832ae110f54739109f985734e803
romtuck2 <tarkin20@yandex.ru>
2021-06-18 00:35:30 +0500

    Template

--- api/del.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    include('sql.php');
-
-    DelRowMySql($_GET['table'], $_GET['id'], $_GET['Column']);
-
-    header("Location: /lk/".$_GET['table'].".php");
-
-?>
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header("Location:/light/index.php");
-?>
